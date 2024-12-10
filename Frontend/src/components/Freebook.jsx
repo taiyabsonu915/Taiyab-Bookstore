1185103b2e9bb0f025da59b4f5a35236d0ae7f4b
taiyabsonu915 <taiyabsonu915@gmail.com>
2024-12-10 20:11:47 +0530

    Update Freebook.jsx

--- Frontend/src/components/Freebook.jsx
+++ Frontend/src/components/Freebook.jsx
@@ -31,30 +31,29 @@ function Freebook() {
     getBook();
   }, []);
 
-  var settings = {
+   const settings = {
     dots: true,
     infinite: false,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 3,
-    adaptiveHeight: true,
+    slidesToScroll: 1,
     initialSlide: 0,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
+          slidesToScroll: 1,
+          infinite: false,
           dots: true,
         },
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
+          slidesToScroll: 1,
+          dots: true,
         },
       },
       {
@@ -62,10 +61,12 @@ function Freebook() {
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
+          dots: true,
         },
       },
     ],
   };
+
 
   return (
     <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 overflow-hidden">
